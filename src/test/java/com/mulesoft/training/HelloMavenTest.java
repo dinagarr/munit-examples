db5cf88f1c30aeb6041a1906e1b1a24497a5ec9c
Dinagar Ramachandran [DATACOM] <DinagarR@datacom.co.nz>
2017-08-16 12:20:00 +1200

    Add Testcase for outbound property

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,24 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
-	
+
 	@Rule
 	public DynamicPort port = new DynamicPort("http.port");
+
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception {
+		System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Dynamic Port  is : " + port.getNumber());
+		runFlowAndExpect("mavensetupFlow", "Hello Maven");
+	}
 	
-    @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception {    	
-    	System.out.println(" >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Dynamic Port  is : " + port.getNumber() );
-        runFlowAndExpect("mavensetupFlow", "Hello Maven");
-    }
-    
-    @Override
-    protected String[] getConfigFiles() {
-    	String[] files = {"mavensetup.xml", "global.xml"};
-        return files;
-    }
+	@Test
+	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		MuleEvent event = runFlow("mavensetupFlow");
+		String contentType = event.getMessage().getOutboundProperty("Content-Type");
+		assertEquals("application/json", contentType);
+	}
+
+	@Override
+	protected String[] getConfigFiles() {
+		String[] files = { "mavensetup.xml", "global.xml" };
+		return files;
+	}
 
 }
